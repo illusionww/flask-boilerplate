a17d91127fbf2e64dbefa410a7ec273e68db2806
Vladimir Ivashkin <illusionww@users.noreply.github.com>
2018-01-15 17:02:18 +0300

    initial code

---
+++ app/app.py
@@ -0,0 +1,90 @@
+import logging
+import os
+from logging.handlers import RotatingFileHandler
+
+from flask import Flask, request, flash, url_for, redirect, render_template, g
+from flask_login import login_user, logout_user, login_required, LoginManager, current_user
+
+from models import db, User
+
+app = Flask(__name__)
+app.secret_key = 'super secret string 228'
+
+file_handler = RotatingFileHandler('output.log', maxBytes=100 * 1024 * 1024, backupCount=5, encoding='utf8')
+file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s'))
+file_handler.setLevel(logging.INFO)
+app.logger.setLevel(logging.INFO)
+app.logger.addHandler(file_handler)
+
+POSTGRES = {
+    'user': os.environ['POSTGRES_USER'],
+    'pw': os.environ['POSTGRES_PASSWORD'],
+    'db': os.environ['POSTGRES_DB'],
+    'host': 'db',
+    'port': '5432',
+}
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+@app.route('/')
+@login_required
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+
+    username = request.form['username']
+    password = request.form['password']
+    remember_me = False
+    if 'remember_me' in request.form:
+        remember_me = True
+    registered_user = User.query.filter_by(username=username, password=password).first()
+    if registered_user is None:
+        flash('Username or Password is invalid', 'error')
+        return redirect(url_for('login'))
+    login_user(registered_user, remember=remember_me)
+    flash('Logged in successfully')
+    return redirect(request.args.get('next') or url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    user = User(request.form['username'], request.form['password'], request.form['email'])
+    db.session.add(user)
+    db.session.commit()
+    flash('User successfully registered')
+    return redirect(url_for('login'))
+
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+@app.before_request
+def before_request():
+    g.user = current_user
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have logged out')
+    return redirect(url_for('login'))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
